bee42af3af91b9b8713f524597abb87e9382f114
u136450 <u136450@straumann.com>
2025-02-26 17:28:21 +0530

    added fix

--- src/components/landing-page-components/upcoming-adventures/upcoming-adventures.tsx
+++ src/components/landing-page-components/upcoming-adventures/upcoming-adventures.tsx
@@ -25,7 +25,7 @@ const UpcomingAdventures = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 sm:px-6 mx-auto align-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 sm:px-6 mx-auto align-center place-items-center ">
         {upcomingAdventures.map((adventure) => (
           <AdventureCard
             title="Biking Adventure"
