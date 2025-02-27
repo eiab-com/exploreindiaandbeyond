341775ef7e06a9b982377f6552ddc739857675d7
nischalAtfrog <nischal.mudennavar@frog.co>
2025-02-27 21:44:20 +0530

    checking

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,9 @@
 import { db } from "./prisma";
-import { revalidateTag } from "next/cache";
+
 
 export const dynamic = "force-dynamic";
 export const getAdventureEnquiries = async () => {
-  revalidateTag("adventureEnquiries"); // Ensure new data is fetched
+ 
   const data = await db.adventureEnquiry.findMany({
     orderBy: {
       createdAt: "desc",
@@ -13,7 +13,7 @@ export const getAdventureEnquiries = async () => {
 };
 
 export const getContacts = async () => {
-  revalidateTag("contacts");
+ 
   const data = await db.contact.findMany({
     orderBy: {
       createdAt: "desc",
