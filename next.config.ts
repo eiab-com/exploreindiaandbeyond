e0ed747ddf760cf623cb811833b28940ef33a59b
u136450 <nischal.mudennavar.ext@straumann.com>
2025-10-20 13:51:16 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,50 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "images.pexels.com" },
-      { protocol: "https", hostname: "videos.pexels.com" },
-      { protocol: "https", hostname: "www.playbook.com" },
-      { protocol: "https", hostname: "drive.google.com" },
-      { protocol: "https", hostname: "jsccmjobjntyhiiwwdmv.supabase.co" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "videos.pexels.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "www.playbook.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "jsccmjobjntyhiiwwdmv.supabase.co",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
     ],
   },
 };
 
 export default nextConfig;
-
-
